51f3790481f07e536a2ac10e94a017a79b09dd45
gridsyntax-admin <gridsyntax@gmail.com>
2025-12-28 11:20:36 -0800

    Drag and drop functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -22,7 +22,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
-
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -63,6 +64,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart', 'drag(event)');
   // Append
   columnEl.appendChild(listEl);
 
@@ -102,6 +105,36 @@ function updateDOM() {
   // Run getSavedColumns only once, Update Local Storage
 
 
+}
+
+// When item starts dragging
+function drag(e) {
+  draggedItem = e.target;
+  console.log('draggedItem:', draggedItem);
+  // dragging = true;
+}
+
+// When the item enters a column area
+function dragEnter(column) {
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+// Column allows for item to drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// Dropping item in column
+function drop(e) {
+  e.preventDefault();
+  // Remove background color/padding
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  });
+  // Add item to column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 // On Load
